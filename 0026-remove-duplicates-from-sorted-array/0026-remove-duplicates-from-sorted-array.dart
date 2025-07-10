388a98ad0a07f535c73f7021d75d8ed7c5499efd
wackyturtle <wcytutl01@gmail.com>
2025-07-10 10:11:25 +0900

    Time: 0 ms (100%), Space: 149.1 MB (67.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.dart
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.dart
@@ -1,12 +1,13 @@
 class Solution {
   int removeDuplicates(List<int> nums) {
-        int k = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[k - 1]) {
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
+    int a = 1;
+    
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[a - 1]) {
+        nums[a] = nums[i];
+        a++;
+      }
+    }
+    return a;
   }
 }
