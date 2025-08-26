91ae303fbdd47621f530c575e55f8b1db26cacee
wackyturtle <wcytutl01@gmail.com>
2025-08-26 09:47:31 +0900

    Time: 2 ms (100%), Space: 147.6 MB (94.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.dart
+++ 0338-counting-bits/0338-counting-bits.dart
@@ -3,7 +3,7 @@ class Solution {
     List<int> count = List<int>.filled(n + 1, 0);
     int set = 1;
     for (var i = 1; i <= n; i++) {
-      if (set * 2 == i) set = i; // 새 블록 시작(2^k 경계)
+      if (set * 2 == i) set = i;
       count[i] = 1 + count[i - set];
     }
     return count;
