cfeeb19d55b27b123e735f6ce69bc0a6af657060
wackyturtle <wcytutl01@gmail.com>
2025-08-26 09:46:16 +0900

    Time: 1 ms (100%), Space: 151.8 MB (23.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.dart
+++ 0338-counting-bits/0338-counting-bits.dart
@@ -1,11 +1,11 @@
 class Solution {
   List<int> countBits(int n) {
-    List<int> count = List<int>.filled(n + 1, 0);
-    int set = 1;
-    for (var i = 1; i <= n; i++) {
-      if (set * 2 == i) set = i;
-      count[i] = 1 + count[i - set];
+    List<int> dp = List.filled(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+      dp[i] = dp[i >> 1] + (i & 1);
     }
-    return count;
+
+    return dp;
   }
 }
